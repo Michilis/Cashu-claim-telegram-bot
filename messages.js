2d64aa399850cebb4235e6d0a79efe99183d0d61
Michilis <120072772+Michilis@users.noreply.github.com>
2024-05-17 01:50:03 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -5,7 +5,7 @@ ${username} shared a Cashu token 🥜
 Click here to claim to Lightning: [Claim link](${cashuApiUrl}?token=${token})
   `,
   claimedMessage: (username) => `
-${username} shared a Cashu token 🥜
+@${username} shared a Cashu token 🥜
 
 Cashu token has been claimed ✅
   `,
@@ -13,4 +13,20 @@ Cashu token has been claimed ✅
   checkingTokenStatus: 'Checking token status...',
   tokenStatusButtonPending: 'Token Status: Pending',
   tokenStatusButtonClaimed: 'Token Status: Claimed',
+  helpMessage: `
+Welcome to the Cashu Telegram Bot!
+
+To start, please add your Lightning address (LNURLp format) by using the command:
+  /add your-address@domain.com
+
+Once your address is set, you can send a Cashu token to this bot and it will be automatically sent to your Lightning address.
+  `,
+  startMessage: `
+Welcome! Let's set up your Lightning address.
+
+Please provide your Lightning address (LNURLp format, looks like an email) using the command:
+  /add your-address@domain.com
+
+After setting your address, you can send Cashu tokens to this bot and they will be sent to your Lightning address automatically.
+  `
 };
